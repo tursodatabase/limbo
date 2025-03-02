83fda1cae421f214dadf10ff7ae4c46ce4584c3f
PThorpe92 <preston@unlockedlabs.org>
2025-03-02 17:18:04 -0500

    Work on defragment_page in the BTree, swap out reference counting in buffer pool for unsafecell

--- simulator/runner/file.rs
+++ simulator/runner/file.rs
@@ -1,6 +1,6 @@
 use std::{cell::RefCell, rc::Rc};
 
-use limbo_core::{File, Result};
+use limbo_core::{File, IOBuff, Result};
 pub(crate) struct SimulatorFile {
     pub(crate) inner: Rc<dyn File>,
     pub(crate) fault: RefCell<bool>,
@@ -85,12 +85,7 @@ impl File for SimulatorFile {
         self.inner.pread(pos, c)
     }
 
-    fn pwrite(
-        &self,
-        pos: usize,
-        buffer: Rc<RefCell<limbo_core::Buffer>>,
-        c: limbo_core::Completion,
-    ) -> Result<()> {
+    fn pwrite(&self, pos: usize, buffer: IOBuff, c: limbo_core::Completion) -> Result<()> {
         *self.nr_pwrite_calls.borrow_mut() += 1;
         if *self.fault.borrow() {
             *self.nr_pwrite_faults.borrow_mut() += 1;
