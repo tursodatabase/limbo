29d68eb6c566d7fa0c667f1267654e91ef69c4a8
PThorpe92 <preston@unlockedlabs.org>
2025-04-03 14:04:28 -0400

    Fix cli tests

--- testing/cli_tests/extensions.py
+++ testing/cli_tests/extensions.py
@@ -344,10 +344,10 @@ def test_kv():
     limbo.execute_dot("INSERT INTO other values (23,32,23);")
     limbo.run_test_fn(
         "create virtual table t using kv_store;",
-        lambda res: "Virtual table module not found: kv_store" in res,
+        lambda res: "Module kv_store not found" in res,
     )
     limbo.execute_dot(f".load {ext_path}")
-    limbo.debug_print(
+    limbo.execute_dot(
         "create virtual table t using kv_store;",
     )
     limbo.run_test_fn(
