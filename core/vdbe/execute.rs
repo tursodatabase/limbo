pedrocarlo/turso

Fixed Group By collation

core/vdbe/execute.rs
@@ -347,6 +347,7 @@ pub fn op_compare(
         start_reg_a,
         start_reg_b,
         count,
+        collation,
     } = insn
     else {
         unreachable!("unexpected Insn {:?}", insn)
@@ -354,6 +355,7 @@ pub fn op_compare(
     let start_reg_a = *start_reg_a;
     let start_reg_b = *start_reg_b;
     let count = *count;
+    let collation = collation.unwrap_or_default();
 
     if start_reg_a + count > start_reg_b {
         return Err(LimboError::InternalError(
@@ -365,7 +367,12 @@ pub fn op_compare(
     for i in 0..count {
         let a = state.registers[start_reg_a + i].get_owned_value();
         let b = state.registers[start_reg_b + i].get_owned_value();
-        cmp = Some(a.cmp(b));
+        cmp = match (a, b) {
+            (OwnedValue::Text(left), OwnedValue::Text(right)) => {
+                Some(collation.compare_strings(left.as_str(), right.as_str()))
+            }
+            _ => Some(a.cmp(b)),
+        };
         if cmp != Some(std::cmp::Ordering::Equal) {
             break;
         }

core/vdbe/explain.rs
@@ -140,12 +140,13 @@ pub fn insn_to_str(
                 start_reg_a,
                 start_reg_b,
                 count,
+                collation,
             } => (
                 "Compare",
                 *start_reg_a as i32,
                 *start_reg_b as i32,
                 *count as i32,
-                OwnedValue::build_text(""),
+                OwnedValue::build_text(&format!("k({count}, {})", collation.unwrap_or_default())),
                 0,
                 format!(
                     "r[{}..{}]==r[{}..{}]",

tursodatabase/limbo

core/vdbe/execute.rs
@@ -365,6 +365,7 @@ pub fn op_compare(
         start_reg_a,
         start_reg_b,
         count,
+        collation,
     } = insn
     else {
         unreachable!("unexpected Insn {:?}", insn)
@@ -372,6 +373,7 @@ pub fn op_compare(
     let start_reg_a = *start_reg_a;
     let start_reg_b = *start_reg_b;
     let count = *count;
+    let collation = collation.unwrap_or_default();
 
     if start_reg_a + count > start_reg_b {
         return Err(LimboError::InternalError(
@@ -383,7 +385,12 @@ pub fn op_compare(
     for i in 0..count {
         let a = state.registers[start_reg_a + i].get_owned_value();
         let b = state.registers[start_reg_b + i].get_owned_value();
-        cmp = Some(a.cmp(b));
+        cmp = match (a, b) {
+            (Value::Text(left), Value::Text(right)) => {
+                Some(collation.compare_strings(left.as_str(), right.as_str()))
+            }
+            _ => Some(a.cmp(b)),
+        };
         if cmp != Some(std::cmp::Ordering::Equal) {
             break;
         }

core/vdbe/explain.rs
@@ -141,12 +141,13 @@ pub fn insn_to_str(
                 start_reg_a,
                 start_reg_b,
                 count,
+                collation,
             } => (
                 "Compare",
                 *start_reg_a as i32,
                 *start_reg_b as i32,
                 *count as i32,
-                Value::build_text(""),
+                Value::build_text(&format!("k({count}, {})", collation.unwrap_or_default())),
                 0,
                 format!(
                     "r[{}..{}]==r[{}..{}]",