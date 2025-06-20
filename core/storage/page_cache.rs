69f7d1b3d762b9e47b4bee031878f513453a25d1
Diego Reis <reisdiego144@gmail.com>
2025-06-19 22:27:02 -0300

    Change PageCacheKey to be a custom type

--- core/storage/page_cache.rs
+++ core/storage/page_cache.rs
@@ -8,9 +8,7 @@ use super::pager::PageRef;
 const DEFAULT_PAGE_CACHE_SIZE_IN_PAGES: usize = 2000;
 
 #[derive(Debug, Eq, Hash, PartialEq, Clone)]
-pub struct PageCacheKey {
-    pgno: usize,
-}
+pub struct PageCacheKey(pub usize);
 
 #[allow(dead_code)]
 struct PageCacheEntry {
@@ -58,11 +56,6 @@ pub enum CacheResizeResult {
     PendingEvictions,
 }
 
-impl PageCacheKey {
-    pub fn new(pgno: usize) -> Self {
-        Self { pgno }
-    }
-}
 impl DumbLruPageCache {
     pub fn new(capacity: usize) -> Self {
         assert!(capacity > 0, "capacity of cache should be at least 1");
@@ -567,7 +560,7 @@ impl PageHashMap {
     }
 
     fn hash(&self, key: &PageCacheKey) -> usize {
-        key.pgno % self.capacity
+        key.0 % self.capacity
     }
 
     pub fn rehash(&self, new_capacity: usize) -> PageHashMap {
@@ -596,7 +589,7 @@ mod tests {
     };
 
     fn create_key(id: usize) -> PageCacheKey {
-        PageCacheKey::new(id)
+        PageCacheKey(id)
     }
 
     #[allow(clippy::arc_with_non_send_sync)]
@@ -993,7 +986,7 @@ mod tests {
                 0 => {
                     // add
                     let id_page = rng.next_u64() % max_pages;
-                    let key = PageCacheKey::new(id_page as usize);
+                    let key = PageCacheKey(id_page as usize);
                     #[allow(clippy::arc_with_non_send_sync)]
                     let page = Arc::new(Page::new(id_page as usize));
                     if let Some(_) = cache.peek(&key, false) {
@@ -1017,7 +1010,7 @@ mod tests {
                     let random = rng.next_u64() % 2 == 0;
                     let key = if random || lru.is_empty() {
                         let id_page: u64 = rng.next_u64() % max_pages;
-                        let key = PageCacheKey::new(id_page as usize);
+                        let key = PageCacheKey(id_page as usize);
                         key
                     } else {
                         let i = rng.next_u64() as usize % lru.len();
@@ -1039,7 +1032,7 @@ mod tests {
             for (key, page) in &lru {
                 println!("getting page {:?}", key);
                 cache.peek(&key, false).unwrap();
-                assert_eq!(page.get().id, key.pgno);
+                assert_eq!(page.get().id, key.0);
             }
         }
     }
--- core/storage/pager.rs
+++ core/storage/pager.rs
@@ -648,7 +648,7 @@ impl Pager {
     pub fn read_page(&self, page_idx: usize) -> Result<PageRef, LimboError> {
         tracing::trace!("read_page(page_idx = {})", page_idx);
         let mut page_cache = self.page_cache.write();
-        let page_key = PageCacheKey::new(page_idx);
+        let page_key = PageCacheKey(page_idx);
         if let Some(page) = page_cache.get(&page_key) {
             tracing::trace!("read_page(page_idx = {}) = cached", page_idx);
             return Ok(page.clone());
@@ -749,7 +749,7 @@ impl Pager {
                     let db_size = self.db_header.lock().database_size;
                     for page_id in self.dirty_pages.borrow().iter() {
                         let mut cache = self.page_cache.write();
-                        let page_key = PageCacheKey::new(*page_id);
+                        let page_key = PageCacheKey(*page_id);
                         let page = cache.get(&page_key).expect("we somehow added a page to dirty list but we didn't mark it as dirty, causing cache to drop it.");
                         let page_type = page.get().contents.as_ref().unwrap().maybe_page_type();
                         trace!("cacheflush(page={}, page_type={:?}", page_id, page_type);
@@ -1031,7 +1031,7 @@ impl Pager {
                 page.set_dirty();
                 self.add_dirty(page.get().id);
 
-                let page_key = PageCacheKey::new(page.get().id);
+                let page_key = PageCacheKey(page.get().id);
                 let mut cache = self.page_cache.write();
                 match cache.insert(page_key, page.clone()) {
                     Ok(_) => (),
@@ -1056,7 +1056,7 @@ impl Pager {
             page.set_dirty();
             self.add_dirty(page.get().id);
 
-            let page_key = PageCacheKey::new(page.get().id);
+            let page_key = PageCacheKey(page.get().id);
             let mut cache = self.page_cache.write();
             match cache.insert(page_key, page.clone()) {
                 Err(CacheError::Full) => Err(LimboError::CacheFull),
@@ -1074,7 +1074,7 @@ impl Pager {
         page: PageRef,
     ) -> Result<(), LimboError> {
         let mut cache = self.page_cache.write();
-        let page_key = PageCacheKey::new(id);
+        let page_key = PageCacheKey(id);
 
         // FIXME: use specific page key for writer instead of max frame, this will make readers not conflict
         assert!(page.is_dirty());
@@ -1332,13 +1332,13 @@ mod tests {
             let cache = cache.clone();
             std::thread::spawn(move || {
                 let mut cache = cache.write();
-                let page_key = PageCacheKey::new(1);
+                let page_key = PageCacheKey(1);
                 cache.insert(page_key, Arc::new(Page::new(1))).unwrap();
             })
         };
         let _ = thread.join();
         let mut cache = cache.write();
-        let page_key = PageCacheKey::new(1);
+        let page_key = PageCacheKey(1);
         let page = cache.get(&page_key);
         assert_eq!(page.unwrap().get().id, 1);
     }
