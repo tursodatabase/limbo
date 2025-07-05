6d3cb4d8086edbdba07018bd9f332e38816a94fe
Levy A. <levyddsa@gmail.com>
2025-07-05 02:15:24 -0300

    modify tests for new formatter

--- vendored/sqlite3-parser/src/to_sql_string/stmt/create_trigger.rs
+++ vendored/sqlite3-parser/src/to_sql_string/stmt/create_trigger.rs
@@ -256,7 +256,7 @@ mod tests {
          BEGIN
              INSERT INTO employee_log (action, employee_id, timestamp)
              VALUES ('INSERT', NEW.id, CURRENT_TIMESTAMP);
-         END;"
+         END"
     );
 
     to_sql_string_test!(
@@ -267,7 +267,7 @@ mod tests {
         BEGIN
             INSERT INTO employee_log (action, employee_id, old_value, new_value, timestamp)
             VALUES ('UPDATE', OLD.id, OLD.salary, NEW.salary, CURRENT_TIMESTAMP);
-        END;"
+        END"
     );
 
     to_sql_string_test!(
@@ -278,7 +278,7 @@ mod tests {
          BEGIN
              INSERT INTO employee_log (action, employee_id, timestamp)
              VALUES ('DELETE', OLD.id, CURRENT_TIMESTAMP);
-         END;"
+         END"
     );
 
     to_sql_string_test!(
@@ -288,8 +288,8 @@ mod tests {
          FOR EACH ROW
          WHEN NEW.salary < 0
          BEGIN
-             SELECT RAISE(FAIL, 'Salary cannot be negative');
-         END;"
+             SELECT RAISE (FAIL, 'Salary cannot be negative');
+         END"
     );
 
     to_sql_string_test!(
@@ -300,6 +300,6 @@ mod tests {
          BEGIN
              INSERT INTO departments (name) SELECT NEW.department WHERE NOT EXISTS (SELECT 1 FROM departments WHERE name = NEW.department);
              INSERT INTO employees (name, department_id) VALUES (NEW.name, (SELECT id FROM departments WHERE name = NEW.department));
-         END;"
+         END"
     );
 }
